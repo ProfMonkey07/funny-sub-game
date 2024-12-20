d3105c36853d31513a64ca2d6a36acbaf3797554
ProfMonkey07 <67666529+ProfMonkey07@users.noreply.github.com>
2024-12-19 19:21:20 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import sys
 import math
 import pygame
 from pygame.locals import QUIT
-
+import collision
 
 def distance(a, b):
     return math.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2)
 
-
+pygame.font.init()
+my_font = pygame.font.SysFont('Times New Roman', 30)
 pygame.init()
 
 fps = 60
@@ -33,6 +34,8 @@ sub = pygame.Surface((20, 30), pygame.SRCALPHA)
 pygame.draw.ellipse(sub, (255, 255, 255), (0, 0, 20, 30))
 print(pygame.Surface.get_bounding_rect(sub))
 torpedoes = []
+terrain = [[(100, 100), (120, 100)], [(80, 90), (100, 100)], [(80, 90),(120, 100)]]
+
 
 
 class Torpedo:
@@ -47,9 +50,14 @@ class Torpedo:
     def destruct(self):
         del self
 
-
 def anglevector(a, b):
     return (math.cos(a) * b, math.sin(a) * b)
+
+def text(imagedir, t):
+    speech = my_font.render(t, False, (255, 255,255))
+    screen.blit(speech, (100, 400))
+    image = pygame.image.load(imagedir)
+    screen.blit(image, (30, 400))
 
 
 while True:
@@ -88,8 +96,8 @@ while True:
                 Torpedo(
                     [math.cos(rotation) * 2, math.sin(rotation) * 2],
                     [
-                        location[0] + math.cos(rotation - 0.5) * 10,
-                        location[1] - math.sin(rotation - 0.5) * 15,
+                        location[0] + math.cos(rotation) * 10,
+                        location[1] - math.sin(rotation) * 15,
                     ],
                 )
             )
@@ -99,9 +107,24 @@ while True:
 
     for torp in torpedoes:
         torp.move()
+    nocol = True
 
-    location[0] += math.cos(rotation) * speed
-    location[1] -= math.sin(rotation) * speed
+    #defining a hitbox before doing collision detection
+    front = ((math.cos(rotation)*15)+location[0],(math.sin(rotation)*15)+location[1])
+    back = (location[0]-(math.cos(rotation)*15),location[1]-(math.sin(rotation)*15))
+    left = ((math.cos(rotation-90)*10)+location[0],(math.sin(rotation-90)*10)+location[1])
+    right = ((math.cos(rotation+90)*10)+location[0],(math.sin(rotation+90)*10)+location[1])
+    hitbox = [[front, left], [left, back], [back, right], [right, front]]
+    for segment in terrain:
+        for side in hitbox:
+            if collision.collides(side, segment):
+                print("collision!")
+                nocol = False
+    if nocol:
+        location[0] += math.cos(rotation) * speed
+        location[1] -= math.sin(rotation) * speed
+    else:
+        speed=0
     # Draw.
     pygame.draw.circle(screen, (0, 100, 0), location, radar)
     radarvect = anglevector(radarangle, radar)
@@ -111,6 +134,8 @@ while True:
         location,
         (radarvect[0] + location[0], radarvect[1] + location[1]),
     )
+    for segment in terrain:
+        pygame.draw.line(screen, (100, 100, 100), segment[0], segment[1])
     if radarangle == 0:
         if distance(location, enemyloc) <= radar:
             lastknown[0] = enemyloc[0]
@@ -118,12 +143,18 @@ while True:
             inrange = True
         else:
             inrange = False
-
+    #drawing red dot
     if distance(location, lastknown) <= radar and inrange:
         pygame.draw.circle(screen, (255, 0, 0), lastknown, 3)
+    #drawing torpedoes
     for torp in torpedoes:
         pygame.draw.circle(screen, (255, 100, 0), torp.position, 3)
+    #rotate and blit the submarine surface
     rotsub = pygame.transform.rotate(sub, math.degrees(rotation) - 90)
     screen.blit(rotsub, (location[0] - 10, location[1] - 15))
+    #text display
+    text("eye.png", "THE EYE")
+    for side in hitbox:
+        pygame.draw.line(screen, (0, 0, 255), side[0], side[1])
     pygame.display.flip()
     fpsClock.tick(fps)
